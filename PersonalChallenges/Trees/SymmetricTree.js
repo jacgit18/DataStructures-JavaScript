5c9a3e862efbc9bb18505d72bd8983c2cf377795
jacgit18 <jacdevgoo2020@gmail.com>
2021-05-28 01:42:09 -0400

    added trees

---
+++ PersonalChallenges/Trees/SymmetricTree.js
@@ -0,0 +1,12 @@
+// let treeMirror = [1,2,2,3,4,4,3];
+
+
+const isSymmetric = (root) =>{
+
+console.log(root)
+}
+
+
+// console.log(isSymmetric([1,2,2,3,4,4,3]));
+isSymmetric([1,2,2,3,4,4,3]);
+isSymmetric([1,2,2,null,3,null,3]);
