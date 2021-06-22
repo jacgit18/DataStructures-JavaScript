3ac0a27d444248e0345e11b3ff978cb695eae092
jacgit18 <jacdevgoo2020@gmail.com>
2021-06-22 14:34:37 -0400

    edit

--- PersonalChallenges/Dynamic Programming/LT198_House_Robber.js
+++ PersonalChallenges/Dynamic Programming/LT198_House_Robber.js
@@ -1,12 +1,22 @@
-
-function myrob(){
+/**
+ * 
+ * Input:
+ * Output:
+ * 
+ * Naive Approach 
+   ------------------------
+   Vairiable:
+   
+*/
+const myrob = () => {
     let cache={};
-    return function myrobDP(nums,i){
-        
+    return myrobDP = (nums,i) =>{
+        // test cases
         if(i in cache){
             return cache[i];
         }
         else{
+            // base case
              if(i<0) return 0;
              else{
                  cache[i]=Math.max(myrobDP(nums,i-2)+nums[i],myrobDP(nums,i-1));
@@ -25,3 +35,19 @@ const myrobObj = myrob();
 
 console.log(myrobObj(price,price.length -1))
 // console.log(rob(price))
+
+console.log(`\n ------- Edge Case -------- `);
+
+console.log(` Case 01  ${myrobObj(price,price.length -1)}`)
+
+
+
+/**
+ * 
+ * 
+ * 
+ * Optimal Approach 
+   ------------------------
+   Vairiable:
+   
+*/
